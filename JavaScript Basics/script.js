adf56b8c1c9346932f1ede604feda843bbd9f513
Mehedi Robin <robinicage@gmail.com>
2024-11-22 21:58:58 +0600

    1st Day

---
+++ JavaScript Basics/script.js
@@ -0,0 +1,8 @@
+// alert("Here we go"); html implementation check
+
+// Use of prompt()
+
+// const user = prompt("Hi, What is Your Name?");
+// const greeting = document.getElementsByClassName("text")[0];
+
+// greeting.innerText = "Welcome " + user;
